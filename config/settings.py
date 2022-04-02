0aa5263334316bc2c17573ab0aea88400f398ba4
caecilius21 <ayoub.bouhachmoud@gmail.com>
2022-04-02 04:26:45 +0200

    settings

--- config/settings.py
+++ config/settings.py
@@ -1,34 +1,17 @@
-"""
-Matgit d igenwan? Ad talt allen
-"""
-import os
 from pathlib import Path
 
-import django_heroku
-import dotenv
-import dj_database_url
-
-# Configure Django App for Heroku.
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%v%4k@if=rhcr(6)x@dhv6nx!t1d%x=tzy^f5=#wok1i_n3*c2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['*'] # Set to open for all access
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -40,10 +23,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'django_extensions',
     'backend.names',
     'frontend',
+
 ]
 
 MIDDLEWARE = [
@@ -77,10 +62,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -118,12 +100,4 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 CORS_ORIGIN_ALLOW_ALL = True
-
-
-django_heroku.settings(locals())
-
-# Add these at the very last line of settings.py
-options = DATABASES['default'].get('OPTIONS', {})
-options.pop('sslmode', None)
